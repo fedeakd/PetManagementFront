24c5b170c24d4d98c04e0c1a78019149a170a64f
federico santamaria <soii_fede@hotmail.com>
2023-09-23 20:49:02 -0300

    form generator QR

--- src/app/modules/administration/administration.module.ts
+++ src/app/modules/administration/administration.module.ts
@@ -5,9 +5,10 @@ import { AdministrationRoutingModule } from './administration-routing.module';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
-import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -18,14 +19,14 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     AdministrationRoutingModule,
     MatToolbarModule,
     MatSidenavModule,
     MatButtonModule,
-    MatIconModule,
-    MatDividerModule,
-    MatButtonModule, // Agrega MatInputModule
-    MatIconModule // Agrega MatInputModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class AdministrationModule { }
--- src/app/modules/administration/pages/generate-qr/generate-qr.component.ts
+++ src/app/modules/administration/pages/generate-qr/generate-qr.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-generate-qr',
   templateUrl: './generate-qr.component.html',
   styleUrls: ['./generate-qr.component.css']
 })
-export class GenerateQRComponent {
-  numero!: number;
-  submitForm() {
-    if (this.numero >= 0 && this.numero <= 100) {
-      // Aquí puedes realizar alguna acción con el número válido.
-      console.log('Número válido:', this.numero);
-    } else {
-      console.log('Número fuera de rango');
+export class GenerateQRComponent implements OnInit {
+  myForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.myForm = new FormGroup({
+      countQR: new FormControl([Validators.required, Validators.min(1), Validators.max(100)]),
+    });
+  }
+
+  get f() { return this.myForm.controls; }
+
+
+  onSubmit(form: FormGroup) {
+    if (form.valid) {
+      // Procesa el número aquí
+      console.log('Número ingresado:', form.value.countQR);
     }
   }
 }
