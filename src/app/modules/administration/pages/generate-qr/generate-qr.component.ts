3f06feb843a7d2102cf1235158503e97ccff5ba3
fedeakd <soii_fede@hotmail.com>
2023-09-24 00:25:52 -0300

    generate QR

--- src/app/modules/administration/pages/generate-qr/generate-qr.component.ts
+++ src/app/modules/administration/pages/generate-qr/generate-qr.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-generate-qr',
@@ -8,11 +9,23 @@ import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 })
 export class GenerateQRComponent implements OnInit {
   myForm!: FormGroup;
+  imagenes: any[] = [];
+
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.myForm = new FormGroup({
       countQR: new FormControl([Validators.required, Validators.min(1), Validators.max(100)]),
     });
+
+    const apiKey = 'yQ4vJ4WgpLJzKa-AnmgkzzO5y7gKY5IzkgcTdY0Zvv0';
+
+    // Realiza una solicitud HTTP a la API de Unsplash para obtener imágenes
+    this.http.get<any[]>(`https://picsum.photos/v2/list?page=1&limit=100`)
+      .subscribe(data => {
+        this.imagenes = data;
+      });
   }
 
   get f() { return this.myForm.controls; }
