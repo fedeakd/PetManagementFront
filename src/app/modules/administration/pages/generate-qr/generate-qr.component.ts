45ead08f36a3cc606bb604e046861c0dad4a9443
fedeakd <soii_fede@hotmail.com>
2023-09-26 00:55:50 -0300

    qr

--- src/app/modules/administration/pages/generate-qr/generate-qr.component.ts
+++ src/app/modules/administration/pages/generate-qr/generate-qr.component.ts
@@ -1,8 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { forkJoin } from 'rxjs';
-import { QRService } from 'src/app/shared/service/repository/QR.service';
+import { QRService } from 'src/app/shared/service/repository/qr.service';
 
 @Component({
   selector: 'app-generate-qr',
@@ -16,7 +17,8 @@ export class GenerateQRComponent implements OnInit {
 
 
   constructor(private qrService: QRService,
-    private routeActive: ActivatedRoute,
+    private http: HttpClient,
+     private routeActive: ActivatedRoute,
     private router: Router,) { }
   ngOnInit(): void {
 
@@ -33,10 +35,10 @@ export class GenerateQRComponent implements OnInit {
     const apiKey = 'yQ4vJ4WgpLJzKa-AnmgkzzO5y7gKY5IzkgcTdY0Zvv0';
 
     // Realiza una solicitud HTTP a la API de Unsplash para obtener imágenes
-    /*   this.http.get<any[]>(`https://picsum.photos/v2/list?page=1&limit=100`)
+      this.http.get<any[]>(`https://picsum.photos/v2/list?page=1&limit=100`)
          .subscribe(data => {
            this.imagenes = data;
-         });*/
+         });
   }
 
   get f() { return this.myForm.controls; }
