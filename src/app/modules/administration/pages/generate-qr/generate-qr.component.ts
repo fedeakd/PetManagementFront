60d93fc71235cecb705bb81508a877e3a009a949
fedeakd <soii_fede@hotmail.com>
2023-10-05 00:08:20 -0300

    generate QRs ok

--- src/app/modules/administration/pages/generate-qr/generate-qr.component.ts
+++ src/app/modules/administration/pages/generate-qr/generate-qr.component.ts
@@ -4,6 +4,8 @@ import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { forkJoin } from 'rxjs';
 import { QRService } from 'src/app/shared/service/repository/qr.service';
+import * as JSZip from 'jszip';
+import { saveAs, encodeBase64 } from '@progress/kendo-file-saver';
 
 @Component({
   selector: 'app-generate-qr',
@@ -23,10 +25,30 @@ export class GenerateQRComponent implements OnInit {
   ngOnInit(): void {
 
     forkJoin({
-      listHolidayCountry: this.qrService.get(123),
+     // listHolidayCountry: this.qrService.get(123),
+      list: this.qrService.generate(),
     }).subscribe(data => {
-      console.log(data);
-    });
+      const jszip = new JSZip();
+      for(let i = 0; i < data.list.length; i++) {
+        var binary = atob(data.list[i].fileContents);
+        var array = [];
+
+        for (let j = 0; j < binary.length; j++) {
+          array.push(binary.charCodeAt(j));
+        }
+
+        let image = new Blob([new Uint8Array(array)], {type:  data.list[i].contentType});
+        
+        jszip.file(`${data.list[i].fileDownloadName}.png`, image)
+
+        if(i === (data.list.length -1)){
+          jszip.generateAsync({ type: 'blob' }).then(function(content) {
+            // see FileSaver.js
+            saveAs(content, 'QRs.zip');
+          });
+        }
+      }
+  });
 
     this.myForm = new FormGroup({
       countQR: new FormControl([Validators.required, Validators.min(1), Validators.max(100)]),
@@ -42,8 +64,7 @@ export class GenerateQRComponent implements OnInit {
   }
 
   get f() { return this.myForm.controls; }
-
-
+ 
   onSubmit(form: FormGroup) {
     if (form.valid) {
       // Procesa el número aquí
--- src/app/shared/service/repository/qr.service.ts
+++ src/app/shared/service/repository/qr.service.ts
@@ -43,4 +43,25 @@ export class QRService extends BaseService {
             )
             .pipe(retry(1));
     }
+
+    generate(): Observable<any> {
+        let list = ["92d241de-1957-4266-b0ff-e328ef91908c.jpg",
+        "6454f0be-0054-426c-a596-04da031050b9.jpg",
+        "4377badc-0d68-43f8-b2f2-a2634e25d751.jpg",
+        "c44e1a12-1e52-4362-a5b7-d11d9a6b0a6a.jpg",
+        "72559e43-37db-4643-81e9-b748513a58fe.jpg",
+        "7e46ff5c-55a0-466a-bdd0-e1837141ec63.jpg",
+        "0674b177-1961-4b09-a392-f670746b40ad.jpg",
+        "707cb076-0cfa-492a-b06d-eec7fe6b64bd.jpg",
+        "b9562d8a-eeda-460b-ab6b-1e71c9091fc5.jpg",
+        "6c64b8b2-966b-4c93-88fb-5d12a41fb52b.jpg"];
+
+        return this.http
+            .post<any>(
+                this.apiURL + "/v1/getList",
+                JSON.stringify(list),
+                this.httpOptions
+            )
+            .pipe(retry(1));
+    }
 }
