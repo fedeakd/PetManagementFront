3398586d74dad0f3105cefda459ba5cae05931da
Federico <fedeakd>
2023-11-26 23:05:43 -0300

    test

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -2,5 +2,8 @@ export const environment = {
   production: false,
   port: 5159,
   URL: `https://localhost`,
+ /* production: true,
+  port: 0,
+  URL: `http://matrix01-001-site1.gtempurl.com`,*/
   name: 'development environment',
 };
