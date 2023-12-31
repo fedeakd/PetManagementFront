2d607d3cfb1228f4557f3a608ab8b70d7e92a0fb
juanpox@gmail.com <juanpox@gmail.com>
2023-12-30 21:19:00 -0300

    fixes

--- src/app/modules/information/components/qr-pet-detail/qr-pet-detail.component.ts
+++ src/app/modules/information/components/qr-pet-detail/qr-pet-detail.component.ts
@@ -2,36 +2,73 @@ import { Component, Input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { forkJoin } from 'rxjs';
 import { PetQRInfotmation } from 'src/app/core/model/entity/qrInformation';
+import { PlacesService } from 'src/app/shared/service/repository/places.service';
 import { QRService } from 'src/app/shared/service/repository/qr.service';
 
 @Component({
   selector: 'app-qr-pet-detail',
   templateUrl: './qr-pet-detail.component.html',
-  styleUrls: ['./qr-pet-detail.component.css']
+  styleUrls: ['./qr-pet-detail.component.css'],
 })
 export class QRPetDetailComponent {
   @Input() petInformation!: PetQRInfotmation;
   formattedDate!: string;
 
-  constructor(private routeActive: ActivatedRoute,
+  constructor(
+    private routeActive: ActivatedRoute,
     private qrService: QRService,
-    private router: Router) {
-
+    private placesService: PlacesService,
+    private router: Router
+  ) {
+    this.placesService.getUserLocation().then(() => {
+      this.sendCoords();
+    });
   }
 
   ngOnInit() {
-    this.formattedDate = new Date(this.petInformation.petBirthdayDate ?? "").toLocaleDateString();
+    this.formattedDate = new Date(
+      this.petInformation.petBirthdayDate ?? ''
+    ).toLocaleDateString();
+
+    console.log(this.placesService);
+  }
+
+  sendCoords() {
+    console.log('1');
+    console.log(this.placesService.userLocation);
+
+    if (
+      this.placesService.userLocation !== undefined &&
+      this.placesService.userLocation[1] !== undefined &&
+      this.placesService.userLocation[0] !== undefined
+    ) {
+      console.log('2');
+      let guid: string = this.routeActive.snapshot.paramMap.get('guidID') ?? '';
+      forkJoin({
+        url: this.qrService.sendEditEmailCoord(
+          guid,
+          this.placesService.userLocation[1],
+          this.placesService.userLocation[0]
+        ),
+      }).subscribe((data) => {
+        console.log(data);
+        console.log(
+          'Mail enviado. Recuerde revisar spam y que puede demorar unas horas en llegar el mail'
+        );
+      });
+    }
   }
 
   requestEditInformation() {
-    let guid: string = this.routeActive.snapshot.paramMap.get('guidID') ?? "";
+    let guid: string = this.routeActive.snapshot.paramMap.get('guidID') ?? '';
 
     forkJoin({
       url: this.qrService.sendEditEmail(guid),
-    }).subscribe(data => {
+    }).subscribe((data) => {
       console.log(data);
-      console.log("Mail enviado. Recuerde revisar spam y que puede demorar unas horas en llegar el mail");
-    },);
+      console.log(
+        'Mail enviado. Recuerde revisar spam y que puede demorar unas horas en llegar el mail'
+      );
+    });
   }
-
 }
--- src/app/shared/service/repository/qr.service.ts
+++ src/app/shared/service/repository/qr.service.ts
@@ -6,129 +6,138 @@ import { RouterConfigQRToken } from 'src/app/core/configuration/routerAssistant'
 import { Observable, retry } from 'rxjs';
 import { PetQRInfotmation } from 'src/app/core/model/entity/qrInformation';
 
-
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class QRService extends BaseService {
+  constructor(
+    protected override http: HttpClient,
+    @Inject(RouterConfigQRToken) protected override router: IRouterConfig
+  ) {
+    super(http, router);
+  }
 
-    constructor(protected override http: HttpClient,
-        @Inject(RouterConfigQRToken) protected override router: IRouterConfig) {
-        super(http, router);
+  getbyGuid(guid?: string): Observable<any> {
+    let params: HttpParams = new HttpParams();
+    if (guid !== undefined && guid !== null) {
+      params = params.append('guid', guid);
     }
 
-    getbyGuid(guid?: string): Observable<any> {
-        let params: HttpParams = new HttpParams();
-        if (guid !== undefined && guid !== null) {
-            params = params.append('guid', guid);
-        }
+    return this.http
+      .get<any>(this.apiURL + '/v1', { params: params })
+      .pipe(retry(1));
+  }
 
-        return this.http
-            .get<any>(
-                this.apiURL + "/v1",
-                { params: params }
-            )
-            .pipe(retry(1));
+  getbyToken(token?: string): Observable<any> {
+    let params: HttpParams = new HttpParams();
+    if (token !== undefined && token !== null) {
+      params = params.append('token', token);
     }
 
-    getbyToken(token?: string): Observable<any> {
-        let params: HttpParams = new HttpParams();
-        if (token !== undefined && token !== null) {
-            params = params.append('token', token);
-        }
+    return this.http
+      .get<any>(this.apiURL + '/v1/gbt', { params: params })
+      .pipe(retry(1));
+  }
 
-        return this.http
-            .get<any>(
-                this.apiURL + "/v1/gbt",
-                { params: params }
-            )
-            .pipe(retry(1));
+  sendEditEmail(guid?: string): Observable<any> {
+    let params: HttpParams = new HttpParams();
+    if (guid !== undefined && guid !== null) {
+      params = params.append('guid', guid);
     }
 
-    sendEditEmail(guid?: string): Observable<any> {
-        let params: HttpParams = new HttpParams();
-        if (guid !== undefined && guid !== null) {
-            params = params.append('guid', guid);
-        }
+    return this.http
+      .get<any>(this.apiURL + '/v1/SendEmail', { params: params })
+      .pipe(retry(1));
+  }
 
-        return this.http
-            .get<any>(
-                this.apiURL + "/v1/SendEmail",
-                { params: params }
-            )
-            .pipe(retry(1));
+  sendEditEmailCoord(guid: string, lat: number, lon: number): Observable<any> {
+    let params: HttpParams = new HttpParams();
+    if (
+      guid !== undefined &&
+      guid !== null &&
+      lat !== undefined &&
+      lat !== null &&
+      lon !== undefined &&
+      lon !== null
+    ) {
+      params = params.append('guid', guid);
+      params = params.append('lat', lat);
+      params = params.append('lon', lon);
     }
 
-    sendEditEmailPost(guid?: string): Observable<any> {
-        let params: HttpParams = new HttpParams();
-        if (guid !== undefined && guid !== null) {
-            params = params.append('guid', guid);
-        }
+    return this.http
+      .get<any>(this.apiURL + '/v1/SendEmail2', { params: params })
+      .pipe(retry(1));
+  }
 
-        return this.http
-            .post<any>(
-                this.apiURL + "/v1/SendEmail",
-                JSON.stringify(guid),
-                this.httpOptions
-            )
-            .pipe(retry(1));
+  //   sendEditEmailPost(guid?: string): Observable<any> {
+  //     let params: HttpParams = new HttpParams();
+  //     if (guid !== undefined && guid !== null) {
+  //       params = params.append('guid', guid);
+  //     }
+
+  //     return this.http
+  //       .post<any>(
+  //         this.apiURL + '/v1/SendEmail',
+  //         JSON.stringify(guid),
+  //         this.httpOptions
+  //       )
+  //       .pipe(retry(1));
+  //   }
+
+  getGenerate(count?: number): Observable<any> {
+    let params: HttpParams = new HttpParams();
+    if (count !== undefined && count !== null) {
+      params = params.append('text', count);
     }
 
-    getGenerate(count?: number): Observable<any> {
-        let params: HttpParams = new HttpParams();
-        if (count !== undefined && count !== null) {
-            params = params.append('text', count);
-        }
+    return this.http
+      .get<any>(
+        this.apiURL + '/v1/generate/' + count
+        //{ params: params }
+      )
+      .pipe(retry(1));
+  }
 
-        return this.http
-            .get<any>(
-                this.apiURL + "/v1/generate/" + count,
-                //{ params: params }
-            )
-            .pipe(retry(1));
-    }
+  CreatePetQrInformation(
+    guid: string,
+    petQrInformation: PetQRInfotmation
+  ): Observable<any> {
+    let json = {
+      guid: guid,
+      petInformation: petQrInformation,
+    };
 
-    CreatePetQrInformation(guid: string, petQrInformation: PetQRInfotmation): Observable<any> {
+    return this.http
+      .post<any>(
+        this.apiURL + '/v1/CreatePetQrInformation',
+        JSON.stringify(json),
+        this.httpOptions
+      )
+      .pipe(retry(1));
+  }
 
-        let json = {
-            guid: guid,
-            petInformation: petQrInformation
-        };
+  EditPetQrInformation(
+    guid: string,
+    petQrInformation: PetQRInfotmation
+  ): Observable<any> {
+    let json = {
+      guid: guid,
+      petInformation: petQrInformation,
+    };
 
+    return this.http
+      .post<any>(
+        this.apiURL + '/v1/EditPetQrInformation',
+        JSON.stringify(json),
+        this.httpOptions
+      )
+      .pipe(retry(1));
+  }
 
-        return this.http
-            .post<any>(
-                this.apiURL + "/v1/CreatePetQrInformation",
-                JSON.stringify(json),
-                this.httpOptions
-            )
-            .pipe(retry(1));
-    }
-
-    EditPetQrInformation(guid: string, petQrInformation: PetQRInfotmation): Observable<any> {
-
-        let json = {
-            guid: guid,
-            petInformation: petQrInformation
-        };
-
-        return this.http
-            .post<any>(
-                this.apiURL + "/v1/EditPetQrInformation",
-                JSON.stringify(json),
-                this.httpOptions
-            )
-            .pipe(retry(1));
-    }
-
-    generate(count: number): Observable<any> {
-
-        return this.http
-            .post<any>(
-                this.apiURL + "/v1",
-                JSON.stringify(count),
-                this.httpOptions
-            )
-            .pipe(retry(1));
-    }
+  generate(count: number): Observable<any> {
+    return this.http
+      .post<any>(this.apiURL + '/v1', JSON.stringify(count), this.httpOptions)
+      .pipe(retry(1));
+  }
 }
